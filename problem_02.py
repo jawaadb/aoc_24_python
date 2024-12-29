bd48cf35dde2ac11d1c81d61ad33caaa5386bd03
Jawaad Bhatti <javb222@hotmail.com>
2024-12-29 18:20:01 +0000

    Part 1 solved

--- problem_02.py
+++ problem_02.py
@@ -1,23 +1,23 @@
 import numpy as np
-from helpers import load_matrix
+from helpers import load_text, line_to_numbers
 
-FILE_PATH = "problem_02_example.txt"
+FILE_PATH = "problem_02_data.txt"
 
 
 def main():
-    numbers = load_matrix(FILE_PATH)
+    lines = [line for line in load_text(FILE_PATH) if line != ""]
+    reports = [np.array(line_to_numbers(line), dtype=np.int32) for line in lines]
 
-    num_reports = numbers.shape[0]
-    num_levels = numbers.shape[1]
+    num_reports = len(reports)
+
+    print(f"{num_reports=}")
 
     def check_if_safe(report: np.ndarray) -> bool:
-        assert report.size == num_levels
         diff = np.diff(report)
 
         # all increasing or all decreasing
         all_increasing = np.all(diff >= 0)
         all_decreasing = np.all(diff <= 0)
-
         if not (all_increasing or all_decreasing):
             return False
 
@@ -25,14 +25,11 @@ def main():
         diff_abs = np.abs(diff)
         return np.all((diff_abs >= 1) & (diff_abs <= 3))
 
-    is_safe = np.array(
-        [check_if_safe(numbers[idx_report, :]) for idx_report in range(num_reports)],
-        np.bool,
-    )
+    is_safe = np.array([check_if_safe(report) for report in reports], np.bool)
 
     total_safe = np.sum(is_safe)
 
-    print(f"{total_safe=}")
+    print(f"{total_safe=}")  # Answer: 224
 
 
 main()
