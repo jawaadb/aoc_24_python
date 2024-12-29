bd968ac921c2e5f154380cb3d0f98975364ee5a6
Jawaad Bhatti <javb222@hotmail.com>
2024-12-29 18:31:56 +0000

    Part 2 solved

--- problem_02.py
+++ problem_02.py
@@ -6,10 +6,9 @@ FILE_PATH = "problem_02_data.txt"
 
 def main():
     lines = [line for line in load_text(FILE_PATH) if line != ""]
-    reports = [np.array(line_to_numbers(line), dtype=np.int32) for line in lines]
+    reports = [np.array(line_to_numbers(line), dtype=np.int64) for line in lines]
 
     num_reports = len(reports)
-
     print(f"{num_reports=}")
 
     def check_if_safe(report: np.ndarray) -> bool:
@@ -25,11 +24,27 @@ def main():
         diff_abs = np.abs(diff)
         return np.all((diff_abs >= 1) & (diff_abs <= 3))
 
-    is_safe = np.array([check_if_safe(report) for report in reports], np.bool)
+    def check_if_safe_with_dampener(report: np.ndarray) -> bool:
+        # check without removal
+        if check_if_safe(report):
+            return True
+
+        # check with removal
+        indices = np.arange(report.size)
+        for idx in range(report.size):
+            report_sub = report[indices != idx]
+            if check_if_safe(report_sub):
+                return True
+
+        return False
+
+    is_safe = np.array(
+        [check_if_safe_with_dampener(report) for report in reports], np.bool
+    )
 
     total_safe = np.sum(is_safe)
 
-    print(f"{total_safe=}")  # Answer: 224
+    print(f"{total_safe=}")  # Answer: 293
 
 
 main()
