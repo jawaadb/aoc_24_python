37fc0f906d4b6bb6d4f6204e63030e342b8c18f6
Jawaad Bhatti <javb222@hotmail.com>
2024-12-30 15:18:23 +0000

    Problem 5 - Part 2 solved

--- problem_05.py
+++ problem_05.py
@@ -24,27 +24,45 @@ def load_file(file_path):
     return rules, books
 
 
+def check_against_rule(book: np.ndarray, rule: np.ndarray) -> bool:
+    book_indices = np.arange(book.size)
+    idxL = book_indices[book == rule[0]][0]
+    idxR = book_indices[book == rule[1]][0]
+    return idxL < idxR, idxL, idxR
+
+
 def check_against_rules(book: np.ndarray, rules: np.ndarray) -> bool:
     assert isinstance(book, np.ndarray) and isinstance(rules, np.ndarray)
     assert rules.shape[1] == 2
-
-    def check_against_rule(book: np.ndarray, rule: np.ndarray) -> bool:
-        book_indices = np.arange(book.size)
-        idxL = book_indices[book == rule[0]][0]
-        idxR = book_indices[book == rule[1]][0]
-        return idxL < idxR
 
     rules_sub = rules[np.sum(np.isin(rules, book), axis=1) == 2, :]
 
     rule_passed = np.array(
         [
-            check_against_rule(book, rules_sub[irule, :])
+            check_against_rule(book, rules_sub[irule, :])[0]
             for irule in range(rules_sub.shape[0])
         ],
         np.bool,
     )
 
     return np.all(rule_passed)
+
+
+def sort_book(book: np.ndarray, rules: np.ndarray) -> np.ndarray:
+    rules = rules[np.sum(np.isin(rules, book), axis=1) == 2, :]
+
+    all_good = False
+    while all_good == False:
+        all_good = True
+        for irule in range(rules.shape[0]):
+            is_good, idxL, idxR = check_against_rule(book, rules[irule, :])
+            if is_good:
+                continue
+            all_good = False
+            book[idxL], book[idxR] = book[idxR], book[idxL]
+
+    assert check_against_rules(book, rules)
+    return book
 
 
 def main():
@@ -57,9 +75,13 @@ def main():
 
     rules, books = load_file(FILE_PATH)
 
-    correct_books = filter(partial(check_against_rules, rules=rules), books)
+    incorrect_books = filter(
+        lambda book: check_against_rules(book, rules) == False, books
+    )
 
-    print(f"{sum_mid_pages(correct_books)=}")
+    sorted_books = [sort_book(book, rules) for book in incorrect_books]
+
+    print(f"{sum_mid_pages(sorted_books)=}")  # Answer: 4679
 
 
 main()
