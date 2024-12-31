6eb8be2cbb64b288181b8a365d89dba693531c2f
Jawaad Bhatti <javb222@hotmail.com>
2024-12-31 19:08:23 +0000

    Problem 9 - part 2 solved

--- problem_09.py
+++ problem_09.py
@@ -24,29 +24,49 @@ def main():
         return blockmap
 
     blockmap = unpack_diskmap(diskmap)
-    sz_free = np.sum(blockmap == -1)
     indices = np.indices(blockmap.shape)[0]
 
-    def has_gaps(blockmap: np.ndarray) -> bool:
-        return np.any(blockmap[-sz_free:] != -1)
+    def move_file(file_id: int, blockmap: np.ndarray) -> bool:
+        file_mask = blockmap == file_id
+        file_sz = np.sum(file_mask)
 
-    def defrag(blockmap: np.ndarray):
-        idx_dst = indices[blockmap == -1][0]
-        idx_src = indices[blockmap != -1][-1]
-        blockmap[idx_dst], blockmap[idx_src] = blockmap[idx_src], blockmap[idx_dst]
+        # find free space start indices
+        indices_free = indices[blockmap == -1]
+        indices_free_start = indices_free[np.hstack([0, np.diff(indices_free)]) != 1]
+
+        # find free space sizes
+        data_diff = np.diff(np.hstack([indices[blockmap != -1], blockmap.size]))
+        free_sz = data_diff[data_diff != 1] - 1
+
+        # locate first free space big enough
+        free_spaces = np.vstack([indices_free_start, free_sz])
+
+        sufficient_free_spaces = free_spaces[:, free_spaces[1, :] >= file_sz]
+        if sufficient_free_spaces.shape[1] == 0:
+            return False
+
+        idx_dst = sufficient_free_spaces[0, 0]
+        idx_src = indices[file_mask][0]
+
+        if idx_dst >= idx_src:
+            return False
+
+        blockmap[idx_dst : idx_dst + file_sz] = blockmap[idx_src : idx_src + file_sz]
+        blockmap[idx_src : idx_src + file_sz] = -1
+
+        return True
 
     def calc_checksum(blockmap: np.ndarray) -> int:
         mask = blockmap != -1
         return np.sum(indices[mask] * blockmap[mask])
 
-    def disp(blockmap: np.ndarray):
-        print("".join([str(i) if i != -1 else "." for i in blockmap]))
-
-    while has_gaps(blockmap):
-        defrag(blockmap)
+    for file_id in range(blockmap.max(), 0, -1):
+        print(f"{file_id:6d}", end="\r")
+        move_file(file_id, blockmap)
+    print("")
 
     checksum = calc_checksum(blockmap)
-    print(f"{checksum=}")  # Answer: 6154342787400
+    print(f"{checksum=}")  # Answer: 6183632723350
 
 
 main()
