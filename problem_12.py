e065c6acd747e188d87b3f4268f757e108149954
Jawaad Bhatti <javb222@hotmail.com>
2025-01-01 22:43:38 +0000

    Problem 12 - part 2 solved

--- problem_12.py
+++ problem_12.py
@@ -1,20 +1,37 @@
 from helpers import load_text
 import numpy as np
 
-FILE_PATH = "problem_12_example.txt"
-# FILE_PATH = "problem_12_data.txt"
+FILE_PATH = "problem_12_data.txt"
 
 
 def load_map(file_path) -> np.ndarray:
     return np.array([list(l.strip()) for l in load_text(file_path)], dtype="S1")
 
 
-def main():
-    grid = load_map(FILE_PATH)
-    indices = np.indices(grid.shape)
-    nrows, ncols = grid.shape
+def load_part2_examples() -> np.ndarray:
+    fp = "problem_12_example2.txt"
 
-    def calc_cost(grid: np.ndarray):
+    txt = "".join([l for l in load_text(fp)])
+    chunks = txt.split("\n\n")
+
+    examples: list[tuple[int, np.ndarray]] = []
+    for chunk in chunks:
+        lines = chunk.strip().split("\n")
+        answer = int(lines[0])
+        grid = np.array([list(l) for l in lines[1:]], "S1")
+        examples.append((answer, grid))
+
+    return examples
+
+
+def main():
+    def calc_cost(grid: np.ndarray, print_debug=False):
+        if print_debug:
+            print("debug")
+
+        indices = np.indices(grid.shape)
+        nrows, ncols = grid.shape
+
         def flood_fill(
             fill_pt: np.ndarray, fill_mask: np.ndarray, res_buff: np.ndarray
         ):
@@ -83,12 +100,36 @@ def main():
 
             return perimeter
 
+        def count_sides(fill_mask: np.ndarray):
+            pts = np.vstack([indices[0][fill_mask], indices[1][fill_mask]])
+            pt_tl = np.array([pts[0, :].min(), pts[1, :].min()], np.int64)
+            pt_br = np.array([pts[0, :].max(), pts[1, :].max()], np.int64) + [1, 1]
+            sz = pt_br - pt_tl + [2, 2]
+
+            crop = np.zeros(sz, dtype=np.bool)
+            img = fill_mask[pt_tl[0] : pt_br[0], pt_tl[1] : pt_br[1]]
+            crop[1 : sz[0] - 1, 1 : sz[1] - 1] = img
+
+            def count_verts(arr: np.ndarray) -> int:
+                if print_debug:
+                    print(np.diff(np.int64(arr), axis=1))
+
+                hdiff_pos = np.int64(np.diff(np.int64(arr), axis=1) == 1)
+                hdiff_neg = np.int64(np.diff(np.int64(arr), axis=1) == -1)
+                count = 0
+                for icol in range(hdiff_pos.shape[1]):
+                    count += np.sum(np.diff(np.hstack([0, hdiff_pos[:, icol], 0])) == 1)
+                for icol in range(hdiff_neg.shape[1]):
+                    count += np.sum(np.diff(np.hstack([0, hdiff_neg[:, icol], 0])) == 1)
+                return count
+
+            return count_verts(crop) + count_verts(crop.transpose())
+
         parsed = np.zeros(grid.shape, dtype=np.bool_)
-        cost = 0
+        cost_A, cost_B = 0, 0
         i = 0
         while ~np.all(parsed):
             i += 1
-            print(f"{i}", end="\r")
             start_pos = np.array(
                 [indices[0][~parsed][0], indices[1][~parsed][0]], dtype=np.int64
             )
@@ -100,15 +141,40 @@ def main():
             region = np.zeros(grid.shape, dtype=np.bool_)
             flood_fill(start_pos, plant_area, region)
 
-            cost += measure_area(region) * measure_perimeter(region)
+            area, sides, perimeter = (
+                measure_area(region),
+                count_sides(region),
+                measure_perimeter(region),
+            )
+            if print_debug:
+                print(f"A:{area}, S:{sides}")
+            sub_cost_A = area * perimeter
+            sub_cost_B = area * sides
+            cost_A += sub_cost_A
+            cost_B += sub_cost_B
 
             parsed |= region
+
+        return cost_A, cost_B
+
+    # Tests
+    all_correct = True
+    for iex, (answer, grid) in enumerate(load_part2_examples()):
+        print(f"[E.g. {iex}]")
+        calculated_answer = calc_cost(grid)[1]
+        is_correct = answer == calculated_answer
+        print(f"  {answer=}, {'correct' if is_correct else str(calculated_answer)}")
+        all_correct &= is_correct
+    if not all_correct:
+        print("\nFAIL\n")
+        return
+    else:
         print("")
 
-        return cost
-
-    cost = calc_cost(grid)
-    print(f"cost: {cost}")  # Answer: 1375574
+    # Results
+    costA, costB = calc_cost(load_map(FILE_PATH))
+    print(f"{costA=}")
+    print(f"{costB=}")  # Answer: 830566
 
 
 main()
