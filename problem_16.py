207f6c73c5a0b50e88b732f23fd4482166333cad
Jawaad Bhatti <javb222@hotmail.com>
2025-01-11 20:09:53 +0000

    Problem 16 - part 2 solved

--- problem_16.py
+++ problem_16.py
@@ -1,11 +1,10 @@
 from helpers import load_grid, disp_grid
 import numpy as np
-from functools import partial
 from typing import Dict
+from dataclasses import dataclass
 
 EG1_PATH = "problem_16_eg1.txt"
 EG2_PATH = "problem_16_eg2.txt"
-EG3_PATH = "problem_16_eg3.txt"
 DATA_PATH = "problem_16_data.txt"
 
 START, END, WALL, SPACE = b"S", b"E", b"#", b"."
@@ -15,28 +14,61 @@ add = lambda p1, p2: (p1[0] + p2[0], p1[1] + p2[1])
 diff = lambda p1, p2: (p1[0] - p2[0], p1[1] - p2[1])
 
 dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+DD = {0: ">", 1: "^", 2: "<", 3: "v"}
 
 
 def main():
-    grid, indices = load_grid(EG1_PATH)
+    grid, indices = load_grid(DATA_PATH)
+
+    score_mem: Dict[tuple[int, int, int], int] = {}
+
+    @dataclass
+    class Path:
+        pts: list[tuple[int, int, int]]
+        score: int
+
+        def append(self, p: tuple[int, int, int]):
+            self.pts.append(p)
+            if len(self.pts) <= 1:
+                self.score = 0
+            else:
+                self.score += 1 + (1000 if self.pts[-1][2] != self.pts[-2][2] else 0)
+            if p not in score_mem:
+                score_mem[p] = self.score
+            else:
+                score_mem[p] = min(self.score, score_mem[p])
+            return self
+
+        def copy(self):
+            return Path(pts=self.pts.copy(), score=self.score)
+
+        def disp(self):
+            gcpy = grid.copy()
+            for p in self.pts:
+                gcpy[*p[:2]] = DD[p[2]].encode("utf-8")
+            disp_grid(gcpy)
 
     start_pos = to_tuple(
         np.hstack([indices[0][grid == START], indices[1][grid == START]])
     )
     start_dir = dirs.index((0, 1))
 
-    paths: list[list[tuple[int, int]]] = [[start_pos + tuple([start_dir])]]
+    paths: list[Path] = [Path(pts=[], score=0).append(start_pos + tuple([start_dir]))]
 
-    def next_pos_options(path: list[tuple[int, int, int]]):
+    best_score: int = None
+
+    def next_point_options(path: Path):
         while True:
-            p, d = path[-1][:2], path[-1][2]
+            if score_mem[path.pts[-1]] < path.score:
+                return []
+
+            p, d = path.pts[-1][:2], path.pts[-1][2]
             if grid[*p] == END:
                 return []
             dir_opp = (d + 2) % 4
             cpds = (add(p, dirs[i]) + tuple([i]) for i in range(4))  # cardinal pts
             cpds = filter(lambda pd: pd[-1] != dir_opp, cpds)  # exclude reversing
             cpds = filter(lambda pd: grid[*pd[:2]] != WALL, cpds)  # exclude walls
-            cpds = filter(lambda pd: pd not in path, cpds)  # exclude revisiting
 
             cpds = list(cpds)
             if len(cpds) == 1:
@@ -44,34 +76,22 @@ def main():
             else:
                 return cpds
 
-    def score_path(path: tuple[int, int, int]) -> int:
-        assert len(path) != 0
-        nsteps = len(path) - 1
-        nturns = sum(
-            0 if path[i][2] == path[i - 1][2] else 1 for i in range(1, len(path))
-        )
-        return nsteps + 1000 * nturns
-
-    best_score = None
-    full_paths = []
+    full_paths: list[Path] = []
     while True:
         ipath = 0
         for _ in range(len(paths)):
-            path = paths[ipath]
-            cposs = next_pos_options(path)
+            path: Path = paths[ipath]
+            cposs = next_point_options(path)
 
             match len(cposs):
                 case 0:
-                    if grid[path[-1][:2]] == END:
-                        this_score = score_path(path)
-                        if best_score is None or this_score < best_score:
-                            best_score = this_score
-                            print(f"new best: {best_score}")
-                        full_paths.append(path)
-                    paths.pop(ipath)
+                    end_path = paths.pop(ipath)
+                    if grid[*end_path.pts[-1][:2]] == END:
+                        if best_score is None or end_path.score < best_score:
+                            best_score = end_path.score
+                            print(f"new best: {best_score}", end="\r")
+                        full_paths.append(end_path)
                 case 1:
-                    path.append(cposs[0])
-                    ipath += 1
                     assert False
                 case _:
                     for i in range(len(cposs) - 1):
@@ -81,15 +101,14 @@ def main():
                     ipath += len(cposs)
 
         if best_score is not None:
-            paths = list(filter(lambda pth: score_path(pth) <= best_score, paths))
+            paths = list(filter(lambda pth: pth.score <= best_score, paths))
         if len(paths) == 0:
             break
-    print("")
 
-    full_paths = list(filter(lambda pth: score_path(pth) <= best_score, full_paths))
+    full_paths = list(filter(lambda pth: pth.score <= best_score, full_paths))
 
     for path in full_paths:
-        for p in path:
+        for p in path.pts:
             grid[*p[:2]] = b"O"
 
     num_best_path_tiles = np.sum(grid == b"O")
